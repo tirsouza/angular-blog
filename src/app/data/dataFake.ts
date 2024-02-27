eb619d9ad20258541e658e2c70e01f1cab8255bc
tirsouza <tiagorodriguesufu@gmail.com>
2024-02-26 22:41:41 -0300

    Update blog

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -8,7 +8,21 @@ export const dataFake = [
   {
     "id": "2",
     "title": "Nova Série Anunciada no Disney +",
-    "description": "bla bla bla bla",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce est augue, bibendum sit amet convallis nec, eleifend commodo metus. Nulla nisi enim, commodo quis enim quis, auctor tincidunt enim. Ut pellentesque odio nisi, et ultrices urna vehicula quis. Sed eu tincidunt mi, non vehicula nunc. Donec commodo turpis diam, vel tempor libero semper laoreet. Nullam eleifend, nibh nec elementum gravida, nisl urna lobortis libero, eu viverra quam risus nec purus. Mauris sodales diam ac sollicitudin semper. Nunc vitae porttitor dolor. In mi nunc, sollicitudin eu orci ac, egestas tincidunt massa. Morbi facilisis, lectus eu blandit tempor, risus urna varius tellus, sit amet bibendum urna justo et lectus. Aliquam eget dui eu dui imperdiet tincidunt a id lorem. Donec urna sapien, interdum vitae tempor eu, viverra ac orci. Nam eget dui orci.",
     "photoCover": "https://sobresagas.com.br/wp-content/uploads/2020/12/Disney.jpg"
+  },
+
+  {
+    "id": "3",
+    "title": "NOVO FILME DO PANTERA NEGRA EM BREVE",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ultrices mauris id enim vestibulum bibendum nec eu lorem. Sed tempor eleifend arcu, dignissim finibus ligula dapibus id. Vivamus lacinia neque nec porta pulvinar. Aenean id bibendum enim. Pellentesque lorem mi, mattis at euismod ut, congue sit amet metus. Proin condimentum blandit dui nec posuere. Nunc sit amet tincidunt justo. Sed hendrerit arcu ac tellus gravida suscipit.",
+    "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTNFeOKJoMW_1g3N3KbLUek_x-Bfl4fewrtQ&usqp=CAU"
+  },
+
+  {
+    "id": "4",
+    "title": "DEMOLIDOR GANHA NOVO TRAJE +",
+    "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ultrices mauris id enim vestibulum bibendum nec eu lorem. Sed tempor eleifend arcu, dignissim finibus ligula dapibus id. Pellentesque lorem mi, mattis at euismod ut, congue sit amet metus. Proin condimentum blandit dui nec posuere. Nunc sit amet tincidunt justo. Sed hendrerit arcu ac tellus gravida suscipit.",
+    "photoCover": "https://spider145hqs.files.wordpress.com/2023/03/destaque_demolidor_vol09_asagadopunhovermelho_panini_08032023.jpg?w=1200"
   }
 ];
